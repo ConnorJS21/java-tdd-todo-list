ecd6ec64803d1910099b0b05d7490fbb0555a78e
Connor Stokes <connorjordanstokes@gmail.com>
2024-08-15 11:54:51 +0100

    Task class built, addTask built in ToDoList, tests for addTask pass

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,12 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TodoList {
+    ArrayList<Task> tasks = new ArrayList<>();
+    public void addTask(String taskName, String taskDesc){
+        Task task = new Task(taskName, taskDesc);
+        tasks.add(task);
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -8,7 +8,12 @@ class TodoListTest {
     public void testAddTask() {
         TodoList toDoList = new TodoList();
         toDoList.addTask("Make coffee", "Put nescafe in cup, follow with milk, sugar and hot water.");
-        Assertions.assertEquals(toDoList.toDoList.get[0], taskName="Make coffee", taskDesc = "Put nescafe in cup, follow with milk, sugar and hot water.");
+        Task task = toDoList.tasks.get(0);
+        System.out.println(task);
+        Assertions.assertEquals(task.taskName, "Make coffee");
+        Assertions.assertEquals(task.taskDesc, "Put nescafe in cup, follow with milk, sugar and hot water.");
+        Assertions.assertEquals(task.taskStatus, "Open");
+       /* Assertions.assertEquals(toDoList.tasks.get(0), "{taskName=\"Make coffee\", taskDesc = \"Put nescafe in cup, follow with milk, sugar and hot water.\"}");*/
 
     }
 }
