5989d348afedba9a4e361b8229e771ee08c36318
Connor Stokes <connorjordanstokes@gmail.com>
2024-08-15 13:51:29 +0100

    searchTask built and passes tests

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -18,11 +18,29 @@ public class TodoList {
                 tasksString = tasksString + "Name: " + task.taskName + " | ";
                 tasksString = tasksString + "Description: " + task.taskDesc + " | ";
                 tasksString = tasksString + "Status: " + task.taskStatus + "\n";
-
             }
         } else{
             tasksString = "There are currently no tasks in your to do list.";
         }
         return tasksString;
+    }
+
+    public String searchTask(String taskName){
+        boolean taskFound = false;
+        String returnString = "This task doesn't exist within this list.";
+        Task task = null;
+        for (int i = 0; i < tasks.size(); i++) {
+            task = tasks.get(i);
+            if(task.taskName.equals(taskName)){
+                taskFound = true;
+                break;
+            }
+        }
+        if(taskFound){
+            returnString = "Name: " + task.taskName + " | ";
+            returnString = returnString + "Description: " + task.taskDesc + " | ";
+            returnString = returnString + "Status: " + task.taskStatus;
+        }
+        return returnString;
     }
 }
